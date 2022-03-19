c952662fcdab787c215b6383c3f5cb5e52bb1ee9
khongminhtn <tuyenminhkhong@live.co.uk>
2022-03-19 09:44:17 +0000

    changed totalPages to use .offsetHeight rather than .clientHeight

--- portfolio-client/src/hooks/useScrollY.js
+++ portfolio-client/src/hooks/useScrollY.js
@@ -36,9 +36,9 @@ const useScrollY = (elementRef, scrollState, touchState) => {
       const { pause, currentPage, duration } = scrollStatesRef.current
 
       // Pages availble to scroll
-      const totalPages = (elementRef.current.clientHeight / window.innerHeight) - 1
+      const totalPages = (elementRef.current.offsetHeight / window.innerHeight) - 1
       dispatch(setTotalPages(totalPages)) // ?
-      console.log(totalPages)
+      console.log('changed')
 
 
       // Do nothing if pause is true
