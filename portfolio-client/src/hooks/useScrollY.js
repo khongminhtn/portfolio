3972833afae615e3540efc0aa0e446a316367e91
khongminhtn <tuyenminhkhong@live.co.uk>
2022-03-19 11:56:11 +0000

    Safari scroll test 1

--- portfolio-client/src/hooks/useScrollY.js
+++ portfolio-client/src/hooks/useScrollY.js
@@ -36,7 +36,7 @@ const useScrollY = (elementRef, scrollState, touchState) => {
       const { pause, currentPage, duration } = scrollStatesRef.current
 
       // Pages availble to scroll
-      const totalPages = (elementRef.current.offsetHeight / window.innerHeight) - 1
+      const totalPages = (elementRef.current.offsetHeight / window.outerHeight) - 1
       dispatch(setTotalPages(totalPages)) // ?
 
 
